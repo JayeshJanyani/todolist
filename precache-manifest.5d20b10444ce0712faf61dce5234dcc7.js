7f203332ef870072b059a81ea73e30039dd9eefe
Jayesh Janyani <jjanyani@oradev.oraclecorp.com>
2020-01-25 16:52:49 +0530

    Updates

---
+++ precache-manifest.5d20b10444ce0712faf61dce5234dcc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4e9fcdc45e60e70356637fa11589f4",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "42703ac3076e7bac2804",
+    "url": "/todolist/static/css/main.abe6d670.chunk.css"
+  },
+  {
+    "revision": "cb394e867ab855a90fe4",
+    "url": "/todolist/static/js/2.7aa98d37.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolist/static/js/2.7aa98d37.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42703ac3076e7bac2804",
+    "url": "/todolist/static/js/main.5e804c7b.chunk.js"
+  },
+  {
+    "revision": "cebccde0f7f76a903cc9",
+    "url": "/todolist/static/js/runtime-main.6fa7f924.js"
+  }
+]);
